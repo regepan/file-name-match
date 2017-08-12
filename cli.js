2e2226e9df45ff25e5814ce55ea87316f25e1f0a
Sasaki Yuto <reggaepunch@gmail.com>
2017-08-12 19:04:17 +0700

    WIP

---
+++ cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+"use strict"
+
+const sgf = require('staged-git-files')
+const symbols = require("log-symbols")
+
+sgf('ACM', (err, files) => {
+    if (err) {
+        console.error(err)
+    }
+
+    var fileNames = []
+
+    const regExp = /.*?\.scss$/
+
+    files.forEach((file) => {
+        if (!file.filename.match(regExp)) {
+            return
+        }
+
+        let scssFilteredFileName = file.filename.split('_').pop().replace('.scss', '')
+
+        fileNames.push(scssFilteredFileName)
+    })
+
+    console.log(fileNames)
+
+    files.forEach((file) => {
+    })
+
+    if (true) {
+      console.error('  ' + symbols['error'], 'file name match error...')
+      process.exit(1)
+    }
+})
